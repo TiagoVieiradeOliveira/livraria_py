51f37d33ce48f0367b147705b683b879deacf828
Tiago Vieira <tiagovieiradeoliveira8@gmail.com>
2026-01-16 22:08:56 -0300

    converter_incluso

--- livraria_py/gerenciar.py
+++ livraria_py/gerenciar.py
@@ -1,32 +1,65 @@
 import os
 
 def criararq():
-    if not os.path.exists("dadosl.txt"):
-        with open("dadosl.txt", "w") as arquivo:
-            arquivo.write("")
-    
+    try:
+        if not os.path.exists("livros.txt"):
+            with open("livros.txt", "w") as arquivo:
+                arquivo.write("")
+        return True
+    except OSError as erro:
+        print(f"Erro ao abrir o arquivo {erro}") 
+        return False
+
 
 def getinfototal(arq):
     names=["Titulo", "Autor", "Paginas", "Data Inicio", "Data Termino"]
+    try:
+        arq=open(arq, "rt")
+        for linha in arq:
+            dado=linha.split(";")
+            for i, tipo in enumerate(dado):
+                print(f"{names[i]}: {tipo}")
+
+    except OSError as erro:
+        print(f"Erro ao abrir o arquivo {erro}")
+
+    except FileNotFoundError:
+        print("Arquivo não encontrado")
+
+    except IndexError:
+        print("Erro nos dados do arquivo")
+
+    arq.close()
+
+
+def escreverarq(arq, dados):
+    try:
+        arq=open(arq, "at")
+        for n, i in enumerate(dados):
+            if n==0:
+                arq.write(f"{i}")
+            else: arq.write(f";{i}")
+        arq.write("\n")
+
+    except OSError as erro:
+        print(f"Erro ao abrir o arquivo {erro}")
+    arq.close()
+
+
+def converter(arq):
+    dados_conv=[]
+    arq_conv=[]
     arq=open(arq, "rt")
     for linha in arq:
-        dado=linha.split(";")
-        for i, tipo in enumerate(dado):
-            print(f"{names[i]}: {tipo}")
+        for dado in linha.split(";"):
+            dados_conv.append(dado)
+        arq_conv.append(dados_conv)
+        dados_conv=[]
+    
+    return arq_conv
 
-    arq.close()
-
-def escreverarq(arq, thing):
-    arq=open(arq, "at")
-    for n, i in enumerate(thing):
-        if n==0:
-            arq.write(f"{i}")
-        else: arq.write(f";{i}")
-    arq.write("\n")
-
-    arq.close()
 
 
 criararq()
-getinfototal("dadosl.txt")
 
+print(converter("livros.txt"))
